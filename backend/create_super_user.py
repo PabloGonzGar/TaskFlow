e9b77f27a4bfeb0f13cf6c80c805781755295955
Pablo Segundo <pasegoga@gmail.com>
2025-06-02 12:26:35 +0200

    correccion nombre variable

--- backend/create_super_user.py
+++ backend/create_super_user.py
@@ -8,12 +8,12 @@ django.setup()
 
 User = get_user_model()
 
-username = "admin"
+name = "admin"
 password = "admin123"
 email = "admin@gmail.com"
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
+if not User.objects.filter(name=name).exists():
+    User.objects.create_superuser(name=name, email=email, password=password)
     print("Superusuario creado")
 else:
     print("El superusuario ya existe")

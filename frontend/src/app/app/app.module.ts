5bb2f64812dbfba5ed3e41c2179236bb5d9d6758
Pablo Segundo <pasegoga@gmail.com>
2025-06-02 17:31:51 +0200

    fixes

--- frontend/src/app/app/app.module.ts
+++ frontend/src/app/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { TaskComponent } from '../private/task/task.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { routes } from '../app.routes'; 
+
 
 
 
@@ -10,7 +14,9 @@ import { TaskComponent } from '../private/task/task.component';
   imports: [
     CommonModule,
     DragDropModule,
-    TaskComponent
+    TaskComponent,
+    BrowserModule,
+    RouterModule.forRoot(routes, { useHash: false })
   ]
 })
 export class AppModule { }

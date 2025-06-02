e075e64b9278c8104f26b16665d848eb62c89031
Pablo Segundo <pasegoga@gmail.com>
2025-06-02 17:10:21 +0200

    fixes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RegisterComponent } from "./public/register/register.component";
 import { LoginComponent } from "./public/login/login.component";
-import { LandingComponent } from './public/landing/landing.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegisterComponent, LoginComponent, LandingComponent],
+  imports: [RouterOutlet, RegisterComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

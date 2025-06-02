82019f3500113289e8ce0176ef4101e51a3d18d8
Pablo Segundo <pasegoga@gmail.com>
2025-06-02 17:14:43 +0200

    redirect

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { RegisterComponent } from "./public/register/register.component";
 import { LoginComponent } from "./public/login/login.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegisterComponent, LoginComponent],
+  imports: [RouterOutlet, RegisterComponent, LoginComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private router: Router){}
+  ngOnInit(){
+    this.router.navigate(['/dashboard'])
+  }
 }
